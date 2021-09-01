019b2ae97e0068a555db33157a8554a59995981c
mtisby <marieltisby1998@gmail.com>
2021-09-01 15:43:16 -0700

    notes

--- part1/rerendering/src/App.js
+++ part1/rerendering/src/App.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const App = (props) => {
-  const {counter} = props
+
+const App = () => {
+  const [ counter, setCounter ] = useState(0)
+
+  const handleClick = () => {
+    console.log('clicked')
+  }
+
   return (
-    <div>{counter}</div>
+    <div>
+      <div>{counter}</div>
+      <button onClick={() => setCounter(counter + 1)}>
+        plus
+      </button>
+    </div>
   )
 }
 
