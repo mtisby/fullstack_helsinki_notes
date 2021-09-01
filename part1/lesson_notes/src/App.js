d32c9cb8752d423b646b4946ba6578d5cf492b0a
mtisby <marieltisby1998@gmail.com>
2021-09-01 13:39:11 -0700

    homework

--- part1/lesson_notes/src/App.js
+++ part1/lesson_notes/src/App.js
@@ -1,18 +1,29 @@
 import React from 'react'
 const Hello = (props) => {
+  const bornYear = () => {
+    const yearNow = new Date().getFullYear()
+    return yearNow - props.age
+  }
+
   return (
     <div>
-      <p>Hello {props.name}</p>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+      <p>So you were probably born in {bornYear()}</p>
     </div>
   )
 }
 
 const App = () => {
+  const data = {
+    name: "Jungkook",
+    age: 24
+  }
+
   return (
     <div>
-      <h1>Greetings</h1>
-      <Hello name="George" />
-      <Hello name="Daisy" />
+      <Hello name={data.name} age={data.age}/>
     </div>
   )
 }
